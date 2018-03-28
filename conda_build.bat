2980c596762c2cb3ce0bb10b90123189c0b04c70
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2018-03-28 13:26:24 +0100

    ENH: Add conda to appveyor
    Add conda to appveyor

---
+++ conda_build.bat
@@ -0,0 +1,9 @@
+if "%CONDA_BUILD%" == "true" (
+    conda install conda-build anaconda
+    if "%CONDA_UPLOAD%" == "true" (
+        conda config --set anaconda_upload yes
+        anaconda login --username %ANACONDA_USERNAME% --password %ANACONDA_PASSWORD%
+    )
+    echo conda build --python %PY_VER% --numpy %CONDA_NUMPY_VERSION% ./conda-recipe
+    conda build --python %PY_VER% --numpy %CONDA_NUMPY_VERSION% ./conda-recipe
+)
