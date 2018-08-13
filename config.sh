25ff4900fc2c909de2c38cd368c6d4592c659159
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2018-08-13 22:53:58 +0100

    Update MB

--- config.sh
+++ config.sh
@@ -6,12 +6,19 @@ function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
 
-    # Work around for statsmodels
+    # Work around for statsmodels when wheel doesn't exist
+    echo "----------------- PRE_BUILD -----------------"
+    echo uname -a
+    uname -a
+    # if [ -z "$IS_OSX" ]; then
+        # apt-get install libfreetype6-dev pkg-config libpng12-dev
+    # fi
     pip install numpy scipy cython  
     if [ -n "$IS_OSX" ]; then
         # Only x86_64 by default
         ARCH_FLAGS=${ARCH_FLAGS:-"-arch x86_64"}
     fi
+    echo "----------------- PRE_BUILD -----------------"
 }
 
 function pip_opts {
@@ -22,5 +29,5 @@ function pip_opts {
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
-    pytest --pyargs arch.tests
+    MPLBACKEND="agg" pytest --pyargs arch.tests
 }
