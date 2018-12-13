ee937f4972acf2a6fff6443347ed539e4bb7f5e6
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2018-12-13 14:40:14 +0000

    Improve conda

--- conda-recipe/bld.bat
+++
@@ -1,3 +0,0 @@
-REM Python command to install
-python setup.py install --single-version-externally-managed --record=record.txt
-if errorlevel 1 exit 1
--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+export MKL_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
 export QT_QPA_PLATFORM=offscreen
 pytest --pyargs arch
