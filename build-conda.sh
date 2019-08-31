0d57376eed3da38056faa420cbdf1c9d260ef35e
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2019-08-31 10:29:37 +0100

    MAINT: Get conda build to work
    Attempt to get conda build to work

--- build-conda.sh
+++ build-conda.sh
@@ -2,7 +2,7 @@
 
 echo "Inside build-conda.sh"
 
-if [[ ${PLAT} == "x86_64" && ${UNICODE_WIDTH} == "32" && ${CONDA_BUILD} == true ]] || [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
+if [[ ${PLAT} == "x86_64" && ${UNICODE_WIDTH} == "32" && ${CONDA_BUILD} == true ]] || [[ ${TRAVIS_OS_NAME} == "osx" && ${CONDA_BUILD} == true ]]; then
     if [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
         wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda3.sh -nv;
     fi
