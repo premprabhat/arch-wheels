f6e403896106975e2e3d3cd1ae4cf0b92dab1c39
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2021-11-19 11:18:21 +0000

    RLS: Test 5.1 release build

--- config.sh
+++ config.sh
@@ -5,19 +5,17 @@
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
-    if [ -n "$IS_OSX" ]; then
-        # Only x86_64 by default
-        ARCH_FLAGS=${ARCH_FLAGS:-"-arch x86_64"}
-    fi
+    :
 }
 
-function pip_opts {
-    # Define extra pip arguments
-    :
+function build_wheel {
+    # Prevent spurious numpy upgrade by using bdist_wheel
+    wrap_wheel_builder build_pip_wheel $@
 }
 
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
+    python -m pip list
     MPLBACKEND="agg" python -c "import arch; arch.test()"
 }
